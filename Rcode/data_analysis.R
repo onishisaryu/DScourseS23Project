83155809fb92fe90a91cbcb04df414ea8205b296
Saryu Onishi <saryu@saryu.local>
2023-05-05 12:08:56 -0500

    May 5 update

--- Rcode/data_analysis.R
+++ Rcode/data_analysis.R
@@ -8,20 +8,19 @@ library(MASS)
 # Stepwise multiple regression #
 ################################
 initial <- lm(suffer_score ~ ., data=run_mod)
-b1      <- step(initial, direction='backward')
+step1   <- step(initial, direction='backward')
+summary(step1)
 
-# results
-m1 <- list()
-m1[['Initial']] <- initial
-m1[['Stepwise a']] <- b1
-modelsummary(m1)
+# # results
+# m1 <- list()
+# m1[['Initial']] <- initial
+# m1[['Stepwise a']] <- b1
+# modelsummary(m1)
 
-vif(initial)
-vif(b1)
-
-ggplot(run_mod, aes(x = pmax(predict(b1),0), y = suffer_score, color = average_heartrate)) +
+ggplot(run_mod, 
+       aes(x = predict(step1), y = suffer_score, color = average_heartrate)) +
   geom_point() +
-  geom_smooth(method = "lm", formula = y ~ poly(x, 1), se = T) +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = T) +
   labs(title = "Predicted vs Actual Suffer Score", x = "Predicted", y = "Actual") +
   scale_color_gradient(low = "green", high = "red")
 
@@ -30,18 +29,57 @@ ggplot(run_mod, aes(x = pmax(predict(b1),0), y = suffer_score, color = average_h
 ###########################
 formula <- as.formula("suffer_score ~ distance + time_minutes + average_speed + total_elevation_gain + average_heartrate + max_heartrate + average_cadence + I(distance^2) + I(time_minutes^2) + I(average_speed^2) + I(total_elevation_gain^2) + I(average_heartrate^2) + I(max_heartrate^2) + I(average_cadence^2)")
 full.model <- lm(formula, data = run_mod)
-step.model <- stepAIC(full.model, direction = "backward")
-summary(step.model)
+step2 <- stepAIC(full.model, direction = "backward")
+summary(step2)
 
-m2 <- list()
-m2[['Initial']] <- full.model
-m2[['Quadratic Stepwise']] <- step.model
+# m2 <- list()
+# m2[['Initial']] <- full.model
+# m2[['Quadratic Stepwise']] <- step.model
+# modelsummary(m2)
 
-modelsummary(m2)
-
-
-ggplot(run_mod, aes(x = pmax(predict(step.model),0), y = suffer_score, color = average_heartrate)) +
+ggplot(run_mod, 
+       aes(x = predict(step2), y = suffer_score, color = average_heartrate)) +
   geom_point() +
-  geom_smooth(method = "lm", formula = y ~ poly(x, 1), se = T) +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = T) +
   labs(title = "Predicted vs Actual Suffer Score", x = "Predicted", y = "Actual") +
   scale_color_gradient(low = "green", high = "red")
+
+#######################
+# Adding interactions #
+#######################
+formula <- 
+  as.formula("suffer_score ~ 
+             distance + I(distance^2) +
+             time_minutes + I(time_minutes^2) + 
+             average_speed + I(average_speed^2) + 
+             total_elevation_gain + 
+             average_heartrate + I(average_heartrate^2) + 
+             max_heartrate + I(max_heartrate^2) + 
+             average_cadence + 
+             distance:time_minutes + 
+             distance:average_heartrate + 
+             distance:max_heartrate + 
+             distance:average_cadence + 
+             time_minutes:average_heartrate + 
+             time_minutes:max_heartrate +
+             time_minutes:average_cadence")
+model <- lm(formula, data = run_mod)
+
+# Perform stepwise regression
+step3 <- stepAIC(model, direction = "backward")
+summary(step3)
+
+ggplot(run_mod, aes(x = predict(step3), y = suffer_score, color = average_heartrate)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = T) +
+  labs(title = "Predicted vs Actual Suffer Score", x = "Predicted", y = "Actual") +
+  scale_color_gradient(low = "green", high = "red")
+
+#################
+# Final summary #
+#################
+final <- list()
+final[['model 1']] <- step1
+final[['model 2']] <- step2
+final[['model 3']] <- step3
+modelsummary(final, stars=T, statistic = 'p.value',output = 'latex')
